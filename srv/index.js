51c50abac9fdda866bd6302aa7ede809e6255359
Ilya Chelyshkov <ilya.chalyshkou@leverx.com>
2023-09-07 10:48:21 +0000

    feat: connected to BP API Sandbox env (on SAP Business Accelerator Hub)
    * added .env to .gitignore file;
    * updated package.json with necessary dependencies;
    * updated package.json with the "cds" section:
    (contains information about cap project sucsh as apis, dbs, uaa servicess and so on);
    * updatd index.js file with the handling
    BusinessPartners end-point GET requests
    (implemented proxy wich loads business partners from external services and
    sends in back as an answer to the request);

--- srv/index.js
+++ srv/index.js
@@ -1,39 +1,47 @@
 import cds from "@sap/cds";
 
-function _logReqInfo(req, when) {
-    console.log("==================================");
-    console.log(`When   : ${when}`);
-    console.log(`Method : ${req.method}`);
-    console.log(`Target : ${req.target.name}`);
-    console.log("==================================");
-}
-
 export class RiskService extends cds.ApplicationService {
-    init() {
-        const { Risks, Mitigations } = this.entities;
+    async init() {
+        const BPService = await cds.connect.to("API_BUSINESS_PARTNER");
 
-        this.before("*", (req) => {
-            _logReqInfo(req, "before");
-        });
-        
-        this.on("*", async (req, next) => {
-            _logReqInfo(req, "on");
-            
-            let result = await next();
-            return result;
+        this.on("READ", "BusinessPartners", (req, next) => {
+            return this._readBusinessPartners(req, next, BPService);
         });
         
         this.after("READ", "Risks", (res, req) => {
-            const arr = Array.isArray(res) ? res : [res];
-            arr.forEach(risk => risk.criticality = (risk.impact > 10000 ? 1 : 2));
+            return this._calculateRisksCriticality(res, req);
         });
 
         this.on("error", (err, req) => {
-            if (err.code == "404") {
-                err.message = `[${req.method}]: Can't find ${req.target.name} with specified id: ${req.data.ID}`;
-            }
+            return this._handleError(err, req);
         });
 
         return super.init();
+    }
+
+    async _readBusinessPartners(req, next, service) {
+        req.query.where("LastName <> '' and FirstName <> ''");
+
+        return await service.transaction(req).send({
+            query: req.query,
+            headers: {
+                APIKey: process.env.apikey
+            }
+        });
+    }
+
+    _castToArray(value) {
+        return Array.isArray(value) ? value : [value];
+    }
+
+    _calculateRisksCriticality(res, req) {
+        const arr = this._castToArray(res);
+        arr.forEach(risk => risk.criticality = (risk.impact > 10000 ? 1 : 2));
+    }
+
+    _handleError(err, req) {
+        if (err.code == "404") {
+            err.message = `[${req.method}]: Can't find ${req.target.name} with specified id: ${req.data.ID}`;
+        }
     }
 }
